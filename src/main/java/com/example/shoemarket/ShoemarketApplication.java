c644e1f0e28206ee0618b6e97e917d400ee51aba
Kritsanathon <66011211101@msu.ac.th>
2025-10-31 03:58:42 +0700

    Spring Boot new version (JDBC manual)

---
+++ src/main/java/com/example/shoemarket/ShoemarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoemarketApplication.class, args);
+	}
+
+}
